033d3cdea0d6832abbb6bf4a8b72b14756aae74e
Laila-dAM <lailamota.238@gmail.com>
2024-12-26 14:10:22 -0300

    Functions for word manipulation, menu display, and alphabet generation/storage, with control in the main program flow

--- C/Alien_Alphabet/Alien.c
+++ C/Alien_Alphabet/Alien.c
@@ -24,3 +24,39 @@ typedef struct {
 void generateCharacter(Character *character, int complexity);
 void drawCharacter(char grid[GRID_SIZE][GRID_SIZE]);
 char* applyRules(char* baseWord, int ruleType);
+int validateWord(char* word);
+void showMenu();
+void captureInput();
+void displayAlphabet(Alphabet *alphabet);
+void saveAsText(char* fileName, Alphabet *alphabet);
+void saveAsImage(char* fileName, Alphabet *alphabet);
+
+void clearGrid(char grid[GRID_SIZE][GIRD_SIZE]);
+void printGrid(char grid[GRID_SIZE][GRID_SIZE]);
+
+int main(){
+    Alphabet alphabet;
+    alphabet.numCharacters = 0;
+
+    char choice;
+    do {
+        showMenu();
+        scanf("%c", &choice);
+        switch (choice){
+            case '1' {
+                printf("Enter number of characters to generate (max %d): ", MAX_CHARACTERS);
+        int num;
+        scanf("%d", &num);
+
+        if(num > MAX_CHARACTERS) num = MAX_CHARACTERS;
+        printf("Enter visual complexity (1-10): ");
+        int complexity;
+        scanf("%d", &complexity);
+    for (int i = 0; i < num; i++){
+        generateCharacter(&alphabet.characters[i], complexity);
+    }
+    alphabet.numCharacters = num;
+            }
+        }
+    }
+}
