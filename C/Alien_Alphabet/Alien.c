6c24e0ae312ef9bd45e0d7f13a1ded46ced79993
Laila-dAM <lailamota.238@gmail.com>
2024-12-26 13:53:37 -0300

    Defines structures for characters, alphabets, and words, and declares functions for generating, drawing, and modifying words.

--- C/Alien_Alphabet/Alien.c
+++ C/Alien_Alphabet/Alien.c
@@ -7,3 +7,20 @@
 #define GRID_SIZE 20
 #define MAX_WORD_LENGTH 100
 
+typedef struct {
+    int id;
+    char drawing[GRID_SIZE][GRID_SIZE];
+} Character;
+
+typedef struct {
+    Character characters[MAX_CHARACTERS];
+    int numCharacters;
+} Alphabet;
+
+typedef struct {
+    char content[MAX_WORD_LENGTH];
+} Word;
+
+void generateCharacter(Character *character, int complexity);
+void drawCharacter(char grid[GRID_SIZE][GRID_SIZE]);
+char* applyRules(char* baseWord, int ruleType);
