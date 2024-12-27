f9b65b06214c7dc61760d0408c622abaa88df759
Laila-dAM <lailamota.238@gmail.com>
2024-12-27 15:29:06 -0300

    New files to run the code

--- C/OS/fs.c
+++ C/OS/fs.c
@@ -1,32 +1,34 @@
-#include "types.h"
 #include "kernel.h"
+#include <string.h>   
+#include <stdlib.h>  
+#include <stdint.h> 
 
 #define MAX_FILES 10
-#define MAX_FILE_NAME 255
+#define MAX_FILE_NAME 32
 
 typedef struct {
     char name[MAX_FILE_NAME];
     uint32_t size;
-    uint32_t start_address;
+    uintptr_t start_address;
 } File;
 
 File files[MAX_FILES];
 
-void initialize_filesystem(void) {
-    for(int i = 0; i < MAX_FILES; i++){
+void initialize_filesystem() {
+    for (int i = 0; i < MAX_FILES; i++) {
         files[i].name[0] = '\0';
         files[i].size = 0;
         files[i].start_address = 0;
     }
 }
-int create_file(char* name, uint32_t size){
-    for(int i = 0; i < MAX_FILES; i++){
-        if(files[i].name[0] == '\0'){
+
+void create_file(const char* name, uint32_t size) {
+    for (int i = 0; i < MAX_FILES; i++) {
+        if (files[i].name[0] == '\0') {
             strncpy(files[i].name, name, MAX_FILE_NAME);
             files[i].size = size;
-            files[i].start_address = (uint32_t)malloc(size);
-            return 0;
+            files[i].start_address = (uintptr_t)malloc(size);
+            return;
         }
     }
-    return -1;
 }
--- C/OS/kernel.c
+++ C/OS/kernel.c
@@ -1,16 +1,16 @@
-#include "types.h"
+#include "kernel.h"
 
-void kernel main(void) {
+void kernel_main(void) {
     initialize_interrupts();
     print("Minimal OS Kernel Loaded!");
-    initialize memory();
+    initialize_memory();
 
     while(1){
     }
 }
 void print(char* str){
     char* video_memory = (char*) 0xB8000;
-    for (int i = 0; str[i] ! = '\0'; i++){
+    for (int i = 0; str[i] != '\0'; i++){
         video_memory[i * 2] = str[i];
         video_memory[i * 2 + 1] = 0x0F;
     }
