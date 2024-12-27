979d3ae37618ebcd3915cd3587ba1cd32f22525f
Laila-dAM <lailamota.238@gmail.com>
2024-12-27 14:12:58 -0300

    Displays 'Loading' on the screen

---
+++ C/OS/boot.asm
@@ -0,0 +1,20 @@
+[bits 16]
+[org 0x7C00]
+
+start:
+    mov ah, 0x0E
+    mov al, 'L'
+    int 0x10
+    mov al, 'o'
+    int 0x10
+    mov al, 'a'
+    int 0x10
+    mov al, 'd'
+    int 0x10
+    mov al, 'i'
+    int 0x10
+    mov al, 'n'
+    int 0x10
+    mov al, 'g'
+    int 0x10
+    
