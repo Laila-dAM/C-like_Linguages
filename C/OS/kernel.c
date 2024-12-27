ef43a92fd3f14f62653250841d7cff5bc0be29e9
Laila-dAM <lailamota.238@gmail.com>
2024-12-27 14:24:31 -0300

    Displays text on the screen and calls external functions

--- C/OS/kernel.c
+++ C/OS/kernel.c
@@ -6,6 +6,14 @@ void kernel main(void) {
     initialize memory();
 
     while(1){
-    
     }
 }
+void print(char* str){
+    char* video_memory = (char*) 0xB8000;
+    for (int i = 0; str[i] ! = '\0'; i++){
+        video_memory[i * 2] = str[i];
+        video_memory[i * 2 + 1] = 0x0F;
+    }
+}
+extern void initialize_interrupts(void);
+extern void initialize_memory(void);
