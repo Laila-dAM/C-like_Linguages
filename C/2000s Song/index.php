019bf0907c866940edfa0da04c52a358c794c211
Laila-dAM <lailamota.238@gmail.com>
2024-12-28 14:53:42 -0300

    Displays title and button to start the game

--- C/2000s Song/index.php
+++ C/2000s Song/index.php
@@ -6,4 +6,15 @@
     <title>Guess the 2000s Song</title>
     <link rel="stylesheet" href="style.css">
 </head>
+<body>
+    <h1>Welcome to Guess the 2000s Song!</h1>
+    <button class="button" onclick="startGame()">Start Game</button>
+
+<script>
+    function startGame(){
+        window.location.href = "game.php";
+    }
+</script>
+</body>
+</html>
     
