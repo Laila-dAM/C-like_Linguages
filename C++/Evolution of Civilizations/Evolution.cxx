24f9a4e6f1296002798a436a27974a59e66d4e64
Laila-dAM <lailamota.238@gmail.com>
2024-12-28 17:33:03 -0300

    Defines the Civilization class with attributes to represent a civilization

---
+++ C++/Evolution of Civilizations/Evolution.cxx
@@ -0,0 +1,21 @@
+#include <ionstream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+class Civilization {
+    public:
+    std::string name;
+    int population;
+    int economy;
+    int military;
+    int culture;
+    int politics;
+    int resources;
+    bool isAtWar;
+    bool isAllied;
+    int technology;
+    int environment;
+
+}
