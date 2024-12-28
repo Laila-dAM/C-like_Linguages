34583d3378b82df9acf1297ce5960acd0fbf3672
Laila-dAM <lailamota.238@gmail.com>
2024-12-28 16:49:45 -0300

    Defines song data for a guessing game

---
+++ C/2000s Song/music_data.php
@@ -0,0 +1,254 @@
+<?php
+$musics = [
+    [
+        'emojis' => '🎤💃',
+        'answer' => 'Beyoncé - Single Ladies',
+        'options' => [
+            'Beyoncé - Crazy in Love',
+            'Britney Spears - Toxic',
+            'Shakira - Hips Don’t Lie',
+            'Christina Aguilera - Genie in a Bottle'
+        ]
+    ],
+    [
+        'emojis' => '🚗🎶',
+        'answer' => 'The Offspring - Pretty Fly (for a White Guy)',
+        'options' => [
+            'The Offspring - Pretty Fly (for a White Guy)',
+            'Blink-182 - All the Small Things',
+            'Green Day - Boulevard of Broken Dreams',
+            'Sum 41 - Fat Lip'
+        ]
+    ],
+    [
+        'emojis' => '👑👠',
+        'answer' => 'Hilary Duff - Come Clean',
+        'options' => [
+            'Hilary Duff - Come Clean',
+            'Vanessa Hudgens - Come Back to Me',
+            'Miley Cyrus - Party in the USA',
+            'Lindsay Lohan - Rumors'
+        ]
+    ],
+    [
+        'emojis' => '🎹🎶💔',
+        'answer' => 'Alicia Keys - If I Ain\'t Got You',
+        'options' => [
+            'Alicia Keys - If I Ain\'t Got You',
+            'John Legend - Ordinary People',
+            'Usher - U Got It Bad',
+            'Mariah Carey - We Belong Together'
+        ]
+    ],
+    [
+        'emojis' => '🌙🌌',
+        'answer' => 'Evanescence - My Immortal',
+        'options' => [
+            'Evanescence - My Immortal',
+            'Linkin Park - In the End',
+            'Coldplay - The Scientist',
+            'Simple Plan - I\'m Just a Kid'
+        ]
+    ],
+    [
+        'emojis' => '🔥🎤',
+        'answer' => 'Usher - Yeah!',
+        'options' => [
+            'Usher - Yeah!',
+            'Beyoncé - Irreplaceable',
+            'Alicia Keys - Fallin\'',
+            'Rihanna - Umbrella'
+        ]
+    ],
+    [
+        'emojis' => '🎧🌟',
+        'answer' => 'Daft Punk - One More Time',
+        'options' => [
+            'Daft Punk - One More Time',
+            'The Chemical Brothers - Galvanize',
+            'Fatboy Slim - Praise You',
+            'Justice - D.A.N.C.E.'
+        ]
+    ],
+    [
+        'emojis' => '🌍🎶',
+        'answer' => 'Shakira - Hips Don’t Lie',
+        'options' => [
+            'Shakira - Hips Don’t Lie',
+            'Beyoncé - Single Ladies',
+            'Rihanna - SOS',
+            'Christina Aguilera - Beautiful'
+        ]
+    ],
+    [
+        'emojis' => '💃🕺',
+        'answer' => 'OutKast - Hey Ya!',
+        'options' => [
+            'OutKast - Hey Ya!',
+            'The Black Eyed Peas - Where Is the Love?',
+            'Nelly - Hot in Herre',
+            'Missy Elliott - Work It'
+        ]
+    ],
+    [
+        'emojis' => '🎶🎸',
+        'answer' => 'Green Day - Boulevard of Broken Dreams',
+        'options' => [
+            'Green Day - Boulevard of Broken Dreams',
+            'Blink-182 - All the Small Things',
+            'The Offspring - The Kids Aren\'t Alright',
+            'Good Charlotte - Lifestyles of the Rich & Famous'
+        ]
+    ],
+    [
+        'emojis' => '🏖️🌴',
+        'answer' => 'Limp Bizkit - Rollin\'',
+        'options' => [
+            'Limp Bizkit - Rollin\'',
+            'Korn - Freak on a Leash',
+            'Papa Roach - Last Resort',
+            'P.O.D. - Youth of the Nation'
+        ]
+    ],
+    [
+        'emojis' => '🎤❤️',
+        'answer' => 'Britney Spears - Toxic',
+        'options' => [
+            'Britney Spears - Toxic',
+            'Christina Aguilera - Dirrty',
+            'Shakira - Whenever, Wherever',
+            'Nelly Furtado - I\'m Like a Bird'
+        ]
+    ],
+    [
+        'emojis' => '🌃🎶',
+        'answer' => 'Alicia Keys - No One',
+        'options' => [
+            'Alicia Keys - No One',
+            'Beyoncé - Halo',
+            'Rihanna - Diamonds',
+            'Mariah Carey - We Belong Together'
+        ]
+    ],
+    [
+        'emojis' => '🚴🎵',
+        'answer' => 'Fall Out Boy - Sugar, We\'re Goin Down',
+        'options' => [
+            'Fall Out Boy - Sugar, We\'re Goin Down',
+            'Panic! At The Disco - I Write Sins Not Tragedies',
+            'My Chemical Romance - Welcome to the Black Parade',
+            'Green Day - American Idiot'
+        ]
+    ],
+    [
+        'emojis' => '⚡🎸',
+        'answer' => 'The Killers - Mr. Brightside',
+        'options' => [
+            'The Killers - Mr. Brightside',
+            'Franz Ferdinand - Take Me Out',
+            'Arctic Monkeys - I Bet You Look Good on the Dancefloor',
+            'Kings of Leon - Use Somebody'
+        ]
+    ],
+    [
+        'emojis' => '🍭🎤',
+        'answer' => 'Katy Perry - I Kissed a Girl',
+        'options' => [
+            'Katy Perry - I Kissed a Girl',
+            'Lady Gaga - Poker Face',
+            'Rihanna - Umbrella',
+            'Beyoncé - Crazy in Love'
+        ]
+    ],
+    [
+        'emojis' => '🦄🎤',
+        'answer' => 'Lady Gaga - Just Dance',
+        'options' => [
+            'Lady Gaga - Just Dance',
+            'Kesha - Tik Tok',
+            'Britney Spears - Circus',
+            'Rihanna - Disturbia'
+        ]
+    ],
+    [
+        'emojis' => '🎹🎤',
+        'answer' => 'Christina Aguilera - Beautiful',
+        'options' => [
+            'Christina Aguilera - Beautiful',
+            'Alicia Keys - If I Ain\'t Got You',
+            'Kelly Clarkson - Since U Been Gone',
+            'Leona Lewis - Bleeding Love'
+        ]
+    ],
+    [
+        'emojis' => '💃🎉',
+        'answer' => 'Black Eyed Peas - I Gotta Feeling',
+        'options' => [
+            'Black Eyed Peas - I Gotta Feeling',
+            'LMFAO - Party Rock Anthem',
+            'Pitbull - Give Me Everything',
+            'David Guetta - When Love Takes Over'
+        ]
+    ],
+    [
+        'emojis' => '🎤🎧',
+        'answer' => 'Kanye West - Stronger',
+        'options' => [
+            'Kanye West - Stronger',
+            'Jay-Z - Empire State of Mind',
+            'Drake - Best I Ever Had',
+            'Eminem - Lose Yourself'
+        ]
+    ],
+    [
+        'emojis' => '🎸🎤',
+        'answer' => 'Avril Lavigne - Complicated',
+        'options' => [
+            'Avril Lavigne - Complicated',
+            'Michelle Branch - All You Wanted',
+            'Kelly Clarkson - Breakaway',
+            'Sheryl Crow - Soak Up the Sun'
+        ]
+    ],
+    [
+        'emojis' => '🚀🎶',
+        'answer' => 'Nelly - Hot in Herre',
+        'options' => [
+            'Nelly - Hot in Herre',
+            '50 Cent - In Da Club',
+            'Ludacris - Stand Up',
+            'T.I. - Whatever You Like'
+        ]
+    ],
+    [
+        'emojis' => '🏆🎵',
+        'answer' => 'Eminem - Lose Yourself',
+        'options' => [
+            'Eminem - Lose Yourself',
+            'Jay-Z - 99 Problems',
+            '50 Cent - In Da Club',
+            'Dr. Dre - Still D.R.E.'
+        ]
+    ],
+    [
+        'emojis' => '💔🎧',
+        'answer' => 'Pink - Just Give Me a Reason',
+        'options' => [
+            'Pink - Just Give Me a Reason',
+            'Adele - Someone Like You',
+            'Beyoncé - Irreplaceable',
+            'Kelly Clarkson - Because of You'
+        ]
+    ],
+    [
+        'emojis' => '🌞🎶',
+        'answer' => 'Rihanna - Umbrella',
+        'options' => [
+            'Rihanna - Umbrella',
+            'Beyoncé - Irreplaceable',
+            'Alicia Keys - No One',
+            'Shakira - Hips Don’t Lie'
+        ]
+    ]
+];
+?>
