fc8b76889c60472b73e021e12826d7130c794f0b
Laila-dAM <lailamota.238@gmail.com>
2024-12-28 17:47:32 -0300

    Defines the Civilization class and the evolve method to update attributes based on player choices

--- C++/Evolution of Civilizations/Evolution.cxx
+++ C++/Evolution of Civilizations/Evolution.cxx
@@ -17,5 +17,26 @@ class Civilization {
     bool isAllied;
     int technology;
     int environment;
+Civilization(std::string name)
+        : name(name), population(1000), economy(50), military(50), culture(50), politics(50), resources(100), isAtWar(false), isAllied(false), technology(0), environment(50) {}
 
+void evolve(int politicalChoice, int economicChoice, int culturalChoice, int techChoice, int envChoice){
+    politics += politicalChoice;
+    economy += economicChoice;
+    culture += culturalChoice;
+    population += (economy / 10);
+    resources += (economy / 5);
+    technology += techChoice;
+    environment += envChoice;
+
+if(isWar){
+    military -= (population/50);
+    economy -= 10;
+}
+if(environment < 30){
+    economy -= 5;
+    population -= 10;
+    std::cout << "You environment is in decline, affecting the economy and population!" <<std:endl;
+}
+}
 }
