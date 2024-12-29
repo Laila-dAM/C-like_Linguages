4cafcb23c761c92939e9f5bbd6faa62ac2c60dab
Laila-dAM <lailamota.238@gmail.com>
2024-12-29 11:54:31 -0300

    Moves the player and validates the movement

--- C/Maze_Challenge/kao.c
+++ C/Maze_Challenge/kao.c
@@ -47,5 +47,22 @@ void printLabyrinth(char labyrinth[SIZE][SIZE]){
 int movePlaver(int *x, int *y, char direction, char labyrinth[SIZE][SIZE]) {
     int newX = *x;
     int newY = *y;
-    
+
+if(direction == 'w') newX--;
+    else if(direction == 's') newX++;
+    else if(direction == 'a') newY--;
+    else if(direction == 'd') newY++;
+
+if(newX >= 0 && newX < SIZE && newY >= 0 && newY >= 0 && newY < SIZE && labyrinth[newX][newY] != '|' && labyrinth[newX][newY] !='/' && labyrinth[newX][newY] !='\\' && labyrinth[newX][newY] !='#') {
+    *x = newX;
+    *y = newY;
+    return 1;
+}
+    return 0;
+}
+int main() {
+    int level = 0;
+    int x = 0, y = 0;
+    char direction;
+    int won = 0;
 }
