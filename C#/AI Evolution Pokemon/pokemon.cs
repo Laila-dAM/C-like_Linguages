3f8c085bfe71b5f1ad8268a3558c8305ee83db7d
Laila-dAM <lailamota.238@gmail.com>
2025-01-02 18:11:21 -0300

    Calculates fitness, breeds and mutates Pokémon

--- C#/AI Evolution Pokemon/pokemon.cs
+++ C#/AI Evolution Pokemon/pokemon.cs
@@ -61,7 +61,66 @@ private void EvaluateFitness() {
         pokemon.Fitness = CalculateFitness(pokemon);
     }
         Population = Population.OrderByDescending(p => p.Fitness).Take(Population.Count / 2).ToList();
-
 }
 
+private double CalculateFitness(Pokemon pokemon) {
+    double fitness = 0;
+    if(Climate == "hot") {
+        fitness += pokemon.Speed * 1.5;
+    }
+    else if (Climate == "cold"){
+        fitness += pokemon.Defense * 1.5;
+    }
+    else {
+        fitness += pokemon.Power;
+    }
+    if(ResourceScarcity == "high"){
+        fitness -= pokemon.Speed * 0.5;
+    }
+    else if (ResourceScarcity == "low") {
+        fitness += pokemon.Power * 0.5;
+    }
+    return Math.Max(fitness, 0);
+}
+private void Reproduce() {
+    var newGeneration = new List<Pokemon>();
+    while(newGeneration.Count < Population.Count * 2) {
+        Pokemon parent1 = Population[RandomGenerator.Next(Population.Count)];
+            Pokemon parent2 = Population[RandomGenerator.Next(Population.Count)];
+
+            newGeneration.Add(parent1.Mate(parent2));
+    }
+    Population = newGeneration;
+}
+private void Mutate() {
+    foreach (var pokemon in Population){
+        if(RandomGenerator.NextDouble() < MutationRate) {
+            pokemon.Mutate();
+            
+        }
+    }
+}
+}
+class Pokemon {
+    public int Speed{get; private set;}
+    public int Power {get; private set;}
+    public int Defense {get; private set;}
+    public double Fitness {get; set;}
+    private Random RandomGenerator;
+    public Pokemon(int speed, int power, int defense) {
+        Speed = speed;
+        Power = power;
+        Defense = defense;
+        Fitness = 0;
+        RandomGenerator = new Random ();
+    }
+public Pokemon Mate(Pokemon partner){
+    int newSpeed = (Speed + partner.Speed) / 2;
+    int newPower = (Power + partner.Power) / 2;
+
+    return new Pokemon(newSpeed, newPower, newDefense);
+}
+public void Mutate(){
+    int mutation = RandomGenerator.Next(-10, 11);
+}
 }
